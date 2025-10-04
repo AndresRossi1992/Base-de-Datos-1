4f944c8a2c6104ea4c7841b2ffd3048af014a9a7
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:44:03 -0300

    Delete EJ 2 Consulta 1.sql

--- EJ 2 Consulta 1.sql
+++
@@ -1,6 +0,0 @@
-#Consulta 1: ¿Cuál es el nombre y la dirección de los procuradores que han trabajado en un asunto abierto?
-select Procuradores.nombre, Procuradores.direccion 
-From Procuradores
-join Asuntos_Procuradores on Asuntos_Procuradores.id_procurador = Procuradores.id_procurador
-join Asuntos ON Asuntos.numero_expediente = Asuntos_Procuradores.numero_expediente
-where Asuntos.estado = 'abierto';
