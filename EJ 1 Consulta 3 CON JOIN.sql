c2450bfe4ef91d6cc88b63872dad1ef93da5a1b3
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:40:13 -0300

    Delete EJ 1 Consulta 3 CON JOIN.sql

--- EJ 1 Consulta 3 CON JOIN.sql
+++
@@ -1,5 +0,0 @@
-#Consulta 3 con join: ¿Cuántas salidas ha realizado el barco con matrícula 'ABC123'?
-SELECT COUNT(Salidas.matricula) as Salidas, Salidas.matricula as matricula
-FROM Salidas
-inner Join Barcos on Barcos.matricula = Salidas.matricula
-WHERE Salidas.matricula = 'ABC123';
