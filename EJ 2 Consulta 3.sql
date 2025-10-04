e20e9baaaab266d6f11a704d80f3362eed7e5c4e
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:44:15 -0300

    Delete EJ 2 Consulta 3.sql

--- EJ 2 Consulta 3.sql
+++
@@ -1,5 +0,0 @@
-#Consulta 3: ¿Cuántos asuntos ha gestionado cada procurador?
-SELECT (Procuradores.nombre), Procuradores.id_procurador , COUNT(*) FROM Procuradores
-left join Asuntos_Procuradores on
-Procuradores.id_procurador =  Asuntos_Procuradores.id_procurador
-GROUP BY Procuradores.id_procurador;
