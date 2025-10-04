52d43fca5701d7feef92695b9b56899f9ec187a7
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:44:09 -0300

    Delete EJ 2 Consulta 2.sql

--- EJ 2 Consulta 2.sql
+++
@@ -1,6 +0,0 @@
-#Consulta 2: ¿Qué clientes han tenido asuntos en los que ha participado el procurador Carlos López?
-SELECT Clientes.nombre FROM Clientes
-join Asuntos on Asuntos.dni_cliente = Clientes.dni
-join Asuntos_Procuradores on Asuntos_Procuradores.numero_expediente = Asuntos.numero_expediente
-join Procuradores on Procuradores.id_procurador = Asuntos_Procuradores.id_procurador
-where Procuradores.nombre ='Carlos López';
