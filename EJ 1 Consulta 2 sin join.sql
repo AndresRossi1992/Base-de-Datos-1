765b7c67709fb88da0c48fa444c9c71142f89c61
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:40:06 -0300

    Delete EJ 1 Consulta 2 sin join.sql

--- EJ 1 Consulta 2 sin join.sql
+++
@@ -1,6 +0,0 @@
-#consulta 2 sin join: ¿Cuáles son los nombres de los barcos y sus cuotas de aquellos barcos cuyo socio se llama 'Juan Pérez'?
-SELECT nombre, cuota FROM Barcos
-WHERE id_socio in (
-	SELECT id_socio FROM Socios
-	WHERE nombre = 'Juan Pérez'
-);
