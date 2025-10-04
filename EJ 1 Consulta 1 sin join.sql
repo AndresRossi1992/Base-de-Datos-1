ac047e4789a207cf9dae172c7c4f38e5abfb1bbd
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:38:33 -0300

    Delete EJ 1 Consulta 1 sin join.sql

--- EJ 1 Consulta 1 sin join.sql
+++
@@ -1,6 +0,0 @@
-#consulta 1 sin join: ¿Qué socios tienen barcos amarrados en un número de amarre mayor que 10?
-SELECT nombre FROM Socios
-WHERE id_socio in (
-	SELECT id_socio FROM Barcos
-    WHERE (numero_amarre > 10)
-    );
