cbbec77c4a20f1fc424be3f1e364478147b5d533
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:41:30 -0300

    Delete EJ 1 Consulta 4 sin join.sql

--- EJ 1 Consulta 4 sin join.sql
+++
@@ -1,8 +0,0 @@
-#consulta 4 sin join: Lista los barcos que tienen una cuota mayor a 500 y sus respectivos socios.
-SELECT 
-    Barcos.nombre AS nombre_barco,
-    (SELECT nombre FROM Socios WHERE Socios.id_socio = Barcos.id_socio LIMIT 1) AS nombre_socio
-FROM Barcos
-WHERE Barcos.id_socio IN (
-    SELECT id_socio FROM Barcos WHERE cuota > 500
-);
