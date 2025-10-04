b349a37d6a193a3390d253447b0dfb40c29eb274
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:43:10 -0300

    Delete EJ 1 Consulta 5 CON JOIN.sql

--- EJ 1 Consulta 5 CON JOIN.sql
+++
@@ -1,4 +0,0 @@
-#consulta 5 con join: ¿Qué barcos han salido con destino a 'Mallorca'?
-SELECT Barcos.nombre FROM Barcos
-INNER JOIN Salidas ON Salidas.matricula = Barcos.matricula
-WHERE Salidas.destino = 'Mallorca';
