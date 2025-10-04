9f91a07a8e9974cc1d60ecb36ff6632129dea078
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:44:23 -0300

    Delete EJ 2 Consulta 4.sql

--- EJ 2 Consulta 4.sql
+++
@@ -1,5 +0,0 @@
-#Consulta 4: Lista los números de expediente y fechas de inicio de los asuntos de los clientes que viven en Buenos Aires.
-Select Asuntos.numero_expediente as Num_Expediente, Asuntos.fecha_inicio as Fecha_Inicio
-from Asuntos
-join Clientes on Clientes.dni = Asuntos.dni_cliente
-where direccion like "%Buenos Aires%";
