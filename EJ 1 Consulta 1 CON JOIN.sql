f0878a15f232fb783909ea66c3c65150c879d49c
AndresRossi1992 <38769485+AndresRossi1992@users.noreply.github.com>
2025-10-04 20:38:21 -0300

    Delete EJ 1 Consulta 1 CON JOIN.sql

--- EJ 1 Consulta 1 CON JOIN.sql
+++
@@ -1,5 +0,0 @@
-#Consulta 1 con join: ¿Qué socios tienen barcos amarrados en un número de amarre mayor que 10?
-SELECT Socios.nombre
-FROM Socios
-JOIN Barcos ON Socios.id_socio = Barcos.id_socio
-WHERE Barcos.numero_amarre > 10;
